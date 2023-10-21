7b25e7b53e2d18a4a025a962ef1b4b9b76acd2f9
Lucia <Luciaabouhamad@gmail.com>
2023-10-21 20:47:56 +0200

    Foundation

---
+++ IRDb_LuciaCani/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using IRDb_LuciaCani.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IRDb_LuciaCani.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MovieController : ControllerBase
+	{
+		private readonly IRDbContext _context;
+
+		public MovieController(IRDbContext context) 
+		{
+		     _context = context;
+		}
+	}
+}
